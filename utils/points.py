081f2b9e3f23c05488a76c453a1818d9dd980148
Alex Dutton <aed1988@gmail.com>
2021-10-14 14:40:27 +0100

    Started work on Classes to manager points scoring more easily.

---
+++ utils/points.py
@@ -0,0 +1,50 @@
+points = {
+  'Goalkeeper': {
+    'clean_sheet': 4,
+    'goal': 6,
+    'assist': 3,
+    'appearance': 1,
+    'more_than_sixty_minutes': 1,
+    'yellow_card': -1,
+    'red_card': -2,
+    'every_three_saves': 1,
+    'penalty_save': 5,
+    'penalty_miss': -2,
+    'every_two_goals_conceded': -1,
+    'own_goal': -2
+  },
+  'Defender': {
+    'clean_sheet': 4,
+    'goal': 6,
+    'assist': 3,
+    'appearance': 1,
+    'more_than_sixty_minutes': 1,
+    'yellow_card': -1,
+    'red_card': -2,
+    'penalty_miss': -2,
+    'every_two_goals_conceded': -1,
+    'own_goal': -2
+  },
+  'Midfielder': {
+    'clean_sheet': 1,
+    'goal': 5,
+    'assist': 3,
+    'appearance': 1,
+    'more_than_sixty_minutes': 1,
+    'yellow_card': -1,
+    'red_card': -2,
+    'penalty_miss': -2,
+    'own_goal': -2
+  },
+  'Attacker': {
+    'clean_sheet': 4,
+    'goal': 4,
+    'assist': 3,
+    'appearance': 1,
+    'more_than_sixty_minutes': 1,
+    'yellow_card': -1,
+    'red_card': -2,
+    'penalty_miss': -2,
+    'own_goal': -2
+  }
+}
